65a5b8a597d249db9471039e8fc6da9f8e2ef97a
CSI-Jose-Canto <24-012@sanignacio.pr>
2022-02-28 15:48:00 -0400

    Completamente cambié mi code
    Basicamente empezo y termine un code nuevo. Casi from sratch. Cuando no sabia que code usar busque en YouTube y en google para ver cual seria la mejor forma. Ya no esta pasado en los "possible letters" tiene un filtro de input corto

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,17 +1,7 @@
-from argparse import _AppendAction, _AppendConstAction
 import random
 
 
-steps = ["""
-      _______
-     |/      |
-     |      
-     |      
-     |       
-     |      
-     |
-     |___
-""",
+HangManArt = [
 """
   _______
      |/      |
@@ -22,16 +12,7 @@ steps = ["""
      |
      |___
 """,
-"""
-  _______
-     |/      |
-     |      (_)
-     |       |
-     |       
-     |      
-     |
-     |___
-""",
+
 """
   _______
      |/      |
@@ -81,94 +62,100 @@ steps = ["""
      |      / \\
      |
      |___
-""",
-
+"""
 ]
-wordList = ["CELTICS", "BULLS", "CAVS", "BUCKS", "MAVERICKS", "WARRIORS", "LAKERS", "SPURS", "SIXERS", "CLIPPERS", "PISTONS", "KINGS", "NUGGETS", "HEAT", "SUNS", "NETS", "ROCKETS", "THUNDER", "JAZZ", "HAWKS"]
 
-def get_word():
-     computerChoice = random.choice(wordList)
-     return computerChoice.upper()
+words = ["CELTICS", "BULLS", "CAVS", "BUCKS", "MAVERICKS", "WARRIORS", "LAKERS", "SPURS", "SIXERS", "CLIPPERS", "PISTONS", "KINGS", "NUGGETS", "HEAT", "SUNS", "NETS", "ROCKETS", "THUNDER", "JAZZ", "HAWKS"]
 
-myWord = get_word()
+def get_Word(wordList):
+     # This function returns a random string from the list of strings.
+     #In this case, the computer will choose a random NBA Team
+     wordIndex = random.randint(0, len(wordList) - 1)
+     return wordList[wordIndex]
 
-posibleLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", 'X', "Y", "Z"]
+def display(missedLetters, correctLetters, compWord):
+     print(HangManArt[len(missedLetters)])
+     print()
 
-usedLetters = []
+     print("Used letters:", end=" ")
+     for letter in usedLetters:
+         print(letter, end=' ')
+     print()
 
-def getInput():
-     Letter = input("Choose Letter")
-     while(Letter.upper() not in posibleLetters):
-        print("Error")
-        Letter = input("Choose Letter")        
-     return Letter
+     blanks = "_" * len(compWord)
 
-print("Welcome to Hang Man!")
+     for i in range(len(compWord)): # Replace blanks with correctly guessed letters.
+        if compWord[i] in correctLetters:
+             blanks = blanks[:i] + compWord[i] + blanks[i+1:]
 
-print("Computer selected: " + myWord)
-print(len(myWord)*"_ ")
+     for letter in blanks:          # Show the word with spaces in between each letter.
+         print(letter, end=" ")
+     print()
 
-def getGuess():
+def getGuess(alreadyGuessed):
+                                   # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
      while True:
-          print('Guess a letter.')
-          guess = input()
-          guess = guess.upper()
-          if len(guess) != 1:
-            print('Please enter a single letter.')
-          elif guess in usedLetters:
-               print('You have already guessed that letter. Choose again.')
-          elif guess not in 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z':
-               print('Please enter a LETTER.')
-          else:
-               usedLetters.add(guess)
-               return guess
+         print("Guess a letter.")
+         guess = input()
+         guess = guess.upper()
+         if len(guess) != 1:
+             print("Please enter ONE (1), letter.")
+         elif guess in alreadyGuessed:
+             print("You have already guessed that letter. Choose another one.")
+         elif guess not in 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z':
+             print("Please enter a LETTER, (in british accent) you donut.")
+         else:
+             return guess
 
-def printWord(computerChoice):
-     temp: str=""
-     len(computerChoice)
-     for Letter in myWord:
-          if Letter in usedLetters:
-               temp+=Letter
-          else:
-               temp+="_"
+def playAgain():
+     # This function returns True if the player wants to play again; otherwise, it returns False.
+     #If True then the game will start again, if False the game will end.
+     print("Do you want to play again? (YES or NO)")
+     return input().upper().startswith("Y")
 
-     print(temp)
+print("Hang Man!!")
+usedLetters = " "
+correctLetters = " "
+compWord = get_Word(words)
+gameIsDone = False
 
-# def play(computerChoice):
-#    word_completion = "_ "* len(computerChoice)
-#    print("Computer sslected: " + computerChoice)
-#    guessed = False
-#    guessed_letters = []
-#    tries = 7
-#    print(word_completion)
-#    print("\n")
-#    while not guessed and tries > 0:
-#       guess = input("Guess the word:").upper()
-#       if len(guess) == 1 and guess.isalpha():
-#          if guess in guessed_letters:
-#             print("already used that letter", guess)
-#          elif guess not in computerChoice:
-#             print(guess, "Not the word: (")
-#             tries -= 1
-#             guessed_letters.append(guess)
-#          else:
-#             print("CORRECT", guess, "the letter is in the word")
-#             guessed_letters.append(guess)
-#             word_as_list = list(word_completion)
-#             indices = [i for i, letter in enumerate(computerChoice) if letter == guess]
-#             for index in indices:
-#                word_as_list[index] = guess
-#             word_completion = "".join(word_as_list)
-#             if "_" not in word_completion:
-#                guessed = True
-    
+while True:
+     display(usedLetters, correctLetters, compWord)
 
-print(getInput())
+     # This will let the player enter a letter, the letter is the imput.
+     guess = getGuess(usedLetters + correctLetters)
 
-# def printWord(...)
-#      Temp: Str=""
-#      len(word)
-#      for[Letter in word]
+     if guess in compWord:
+         correctLetters = correctLetters + guess
 
-#      for(incorrect)
-     
+         # This will check if the player has won.
+         foundAllLetters = True
+         for i in range(len(compWord)):
+             if compWord[i] not in correctLetters:
+                 foundAllLetters = False   #No se como hacer que print "Game Over"
+                 break
+         if foundAllLetters:
+             print('CORRECT! The word is "' + compWord +
+                   '"You have won!!')
+             gameIsDone = True
+     else:
+         usedLetters = usedLetters + guess
+
+         # Check if player has guessed too many times and lost.
+         if len(usedLetters) == len(HangManArt) - 1:
+             display(usedLetters, correctLetters, compWord)
+             print("You ran out of guesses!\nAfter " +
+                   str(len(usedLetters)) + "wrong guesses and " +
+                   str(len(correctLetters)) + "correct guesses",
+                   "the word was " + compWord + "")
+             gameIsDone = True
+
+     # Ask the player if they want to play again (but only if the game is done).
+     if gameIsDone:
+         if playAgain():
+             usedLetters = ""
+             correctLetters = ""
+             gameIsDone = False
+             compWord = get_Word(words)
+         else:
+            break
